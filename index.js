2fb2d5da5db1e3c6a044e059dfd31ba7c2379a61
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 15:12:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver
 }
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, [key]: value)
+}
