b536029ac6b5567e37dca1c08cab66ee0c2afb84
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 15:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your solution in this file!
 driver = {}
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver[key] = value;
-  return driver
-}
-
 function updateDriverWithKeyAndValue(driver, key, value) {
   const copy = Object.assign({}, driver, [key]:value)
   return copy
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver
 }
