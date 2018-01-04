cbc6009a634d1a082d313e0ba9740d23a9fe860d
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 15:18:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver
 }
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver
+}
