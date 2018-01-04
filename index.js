c0d32d0378f9f4491a9cd73be695107de664dcdf
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 15:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write your solution in this file!
 driver = {}
 
+function updateDriverWithKeyAndValue(driver, key, value) {
 
+}
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
